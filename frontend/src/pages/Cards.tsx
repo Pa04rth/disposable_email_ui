581565ec16a23a101e3cefcb091caa874a934de7
Harsh22428 <harsh.22428@knit.ac.in>
2025-09-12 17:51:30 +0530

    added contact button

--- frontend/src/pages/Cards.tsx
+++ frontend/src/pages/Cards.tsx
@@ -3,17 +3,17 @@ import { useNavigate } from "react-router-dom";
 
 const cardData = [
   {
-    name: "Netflix",
+    name: "NF",
     color: "bg-red-700",
     img: "/images/netmirror.jpg"
   },
   {
-    name: "Crunchyroll",
+    name: "Crunchy",
     color: "bg-orange-600",
     img: "/images/crunchyroll.png"
   },
   {
-    name: "Youtube Premium",
+    name: "YT Premium",
     color: "bg-red-600",
     img: "/images/youtube.png"
   },
@@ -34,7 +34,21 @@ const Cards = () => {
 
   return (
     <div className="p-8 min-h-screen flex flex-col items-center justify-center bg-gray-50">
-      <h1 className="text-3xl font-bold mb-8">Choose a Service</h1>
+      <h1 className="text-3xl font-bold mb-4">Choose a Service</h1>
+      <div className="flex gap-4 mb-6">
+        <button 
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+          onClick={() => window.open(`https://t.me/Luxuriousdevilott?text=${encodeURIComponent(`Hi I am interested to buy in bulk`)}`, '_blank')}
+        >
+          Buy on Telegram
+        </button>
+        <button 
+          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition"
+          onClick={() => window.open(`https://wa.me/9890938795?text=${encodeURIComponent(`Hi I am interested to buy in bulk`)}`, '_blank')}
+        >
+          Buy on WhatsApp
+        </button>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-2xl">
         {cardData.map((card) => (
           <div
@@ -59,6 +73,14 @@ const Cards = () => {
         >
           <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" className="h-6 w-6" />
           Chat on WhatsApp
+        </button>
+
+        <button
+          className="flex items-center gap-2 px-4 py-2 m-3 bg-telegram  text-white rounded-lg shadow hover:bg-blue-500 transition"
+          onClick={() => window.open(`https://wa.me/9890938795?text=${encodeURIComponent("Hi I am interested to buy in bulk")}`, '_blank')}
+        >
+          <img src="/images/telegram.png" alt="WhatsApp" className="h-6 w-6" />
+          Chat on Telegram
         </button>
       </footer>
     </div>
